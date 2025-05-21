748c317690131ae0f6e33f54b9d1ac8dc7d9667f
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-21 23:53:04 +0530

    Python

---
+++ Python.py
@@ -0,0 +1,5 @@
+#What can Python can do ?
+#Python can be used to create web applications, automate tasks, analyze data, and much more.
+# It is a versatile language that can be used for a wide range of applications.
+#Python is a high-level programming language that is widely used for web development, data analysis, artificial intelligence, scientific computing, and more.
+# It is known for its simplicity and readability, making it a popular choice for beginners and experienced programmers alike.
