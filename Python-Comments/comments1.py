d278f0056f00faa07da0e486ed915c4306b8da28
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-21 23:52:02 +0530

    Python - Comments

---
+++ Python-Comments/comments1.py
@@ -0,0 +1,40 @@
+#TYPES OF COMMENTS IN PYTHON
+# 1. Single-line comments
+# 2. Multi-line comments
+# 3. Docstrings
+
+# 1. Single-line comments
+# This is a single-line comment
+print("Hello, World!")  # This is an inline comment
+
+# 2. Multi-line comments
+'''
+This is a multi-line comment
+It can span multiple lines
+'''
+print("Hello, World!")  # This is an inline comment
+
+# 3. Docstrings
+"""
+This is a docstring
+It is used to document functions, classes, and modules
+"""
+def my_function():
+    """
+    This is a docstring for my_function
+    It describes what the function does
+    """
+    print("Hello, World!")
+my_function()  # Call the function
+# Docstrings can also be used to document classes
+class MyClass:
+    """
+    This is a docstring for MyClass
+    It describes what the class does
+    """
+    def __init__(self):
+        """
+        This is a docstring for the __init__ method
+        It describes what the method does
+        """
+        print("Hello, World!")
